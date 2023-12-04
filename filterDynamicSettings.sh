0cf590b93871a0a7cf0990c5f48f10e0672ecb6f
XoXFaby <xoxfaby@gmail.com>
2023-12-04 08:40:57 +0100

    12/04/2023 08:40:57, File Changed, filterDynamicSettings.sh

--- filterDynamicSettings.sh
+++ filterDynamicSettings.sh
@@ -1,2 +1,2 @@
-sed 's/graphics\\.fov \"70\"/graphics\\.fov \"90\"/g' | \
-sed 's/graphics\\.fov \"70\"/graphics\\.fov \"90\"/g'
+sed 's/graphics\.fov \"70\"/graphics\.fov \"90\"/g' | \
+sed 's/client\.lookatradius \"10\"/client\.lookatradius \"0.01\"/g'
