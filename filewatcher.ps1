4b39d0f20608108ade2aa26718e950932010a866
XoXFaby <xoxfaby@gmail.com>
2023-12-04 07:06:35 +0100

    12/04/2023 07:06:35, File Changed, filewatcher.ps1

--- filewatcher.ps1
+++ filewatcher.ps1
@@ -29,7 +29,7 @@ $Menu_Exit.add_Click({
 ### HIDE SCRIPT AFTER LAUNCH
 $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 $asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
-$null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
+#$null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
 
 ### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
     $watcher = New-Object System.IO.FileSystemWatcher
@@ -55,4 +55,5 @@ $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 
     Register-ObjectEvent $watcher "Changed" -Action $action
     Register-ObjectEvent $watcher "Deleted" -Action $action
     Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {Wait-Event}
 
