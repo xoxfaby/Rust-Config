c47efe0d306a1e23b35174fb745e67b6e7eb96c4
XoXFaby <xoxfaby@gmail.com>
2023-12-04 08:11:22 +0100

    Update filewatcher.ps1

--- filewatcher.ps1
+++ filewatcher.ps1
@@ -1,8 +1,5 @@
-[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')       | out-null
-[System.Reflection.Assembly]::LoadWithPartialName('presentationframework')      | out-null
-[System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')          | out-null
-[System.Reflection.Assembly]::LoadWithPartialName('WindowsFormsIntegration') | out-null
-
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
 $icon = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\Windows\System32\WorkFolders.exe")    
 $Main_Tool_Icon = New-Object System.Windows.Forms.NotifyIcon
@@ -15,12 +12,12 @@ $Menu_Exit.Text = "Exit"
 
 $contextmenu = New-Object System.Windows.Forms.ContextMenu
 $Main_Tool_Icon.ContextMenu = $contextmenu
-$Main_Tool_Icon.contextMenu.MenuItems.AddRange($Menu_Exit)
+$Main_Tool_Icon.ContextMenu.MenuItems.Add($Menu_Exit)
 
-# When Exit is clicked, close everything and kill the PowerShell process
+# When Exit is clicked, raise the "ClosePlease" event
 $Menu_Exit.add_Click({
-    New-Event -SourceIdentifier "ClosePlease" -Sender windows.timer -MessageData "Test"
- })
+    New-Event -SourceIdentifier ClosePlease
+})
 
  
 ### HIDE SCRIPT AFTER LAUNCH
@@ -29,38 +26,30 @@ $asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync
 $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
 
 ### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
-    $watcher = New-Object System.IO.FileSystemWatcher
-    $watcher.Path = $PSScriptRoot
-    $watcher.Filter = "*.*"
-    $watcher.IncludeSubdirectories = $true
-    $watcher.EnableRaisingEvents = $true  
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $PSScriptRoot
+$watcher.Filter = "*.*"
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true  
 
 ### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
-    $action = {
-        $name = $Event.SourceEventArgs.Name 
-        if(-Not $name.StartsWith(".git"))
-        {
-            $changeType = $Event.SourceEventArgs.ChangeType
-            $commitMessage = "$(Get-Date), File $changeType, $name"
-            git add .
-            git commit -m $commitMessage
-            git push
-        }
-    }    
+$action = {
+    $name = $Event.SourceEventArgs.Name 
+    if(-Not $name.StartsWith(".git"))
+    {
+        $changeType = $Event.SourceEventArgs.ChangeType
+        $commitMessage = "$(Get-Date), File $changeType, $name"
+        git add .
+        git commit -m $commitMessage
+        git push
+    }
+}    
 ### DECIDE WHICH EVENTS SHOULD BE WATCHED
-    Register-ObjectEvent $watcher "Created" -Action $action
-    Register-ObjectEvent $watcher "Changed" -Action $action
-    Register-ObjectEvent $watcher "Deleted" -Action $action
-    Register-ObjectEvent $watcher "Renamed" -Action $action
+Register-ObjectEvent $watcher "Created" -Action $action
+Register-ObjectEvent $watcher "Changed" -Action $action
+Register-ObjectEvent $watcher "Deleted" -Action $action
+Register-ObjectEvent $watcher "Renamed" -Action $action
 
 ### WAIT FOR EVENT
-    while ($true) {
-        $res = Wait-Event
-        if($res.SourceIdentifier = "ClosePlease"){
-            $Main_Tool_Icon.Visible = $false
-            $window.Close()
-            Stop-Process $pid
-            break
-        }
-    }
-
+# Wait for the "ClosePlease" event
+Wait-Event -SourceIdentifier ClosePlease
